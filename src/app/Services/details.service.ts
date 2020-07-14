b73a9dd4228bce437b7fe5f78e0faa882e0f9b52
rahul@T90 <rahulpratapsingh2008@gmail.com>
2020-07-14 18:58:54 +0530

    first commit

---
+++ src/app/Services/details.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DetailsService {
+  // private REST_API_SERVER = 'http://localhost:3000';
+  constructor(private httpClient: HttpClient) {
+    this.getDetail().subscribe(data => {
+      // console.log(data);
+  });
+   }
+
+
+
+  // getDetail() {
+  //   return this.httpClient.get('../../Data/data.json');
+  //   // return this.httpClient.get('http://localhost:3000/detail');
+  // }
+
+  public getDetail(): Observable<any> {
+  return this.httpClient.get('../../assets/data.json');
+  }
+
+  // getMoreDetail(data) {
+  //   this.httpClient.get('../../Data/data.json');
+  // }
+}
---
+++ src/app/material.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatCardModule} from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatDividerModule} from '@angular/material/divider';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    // MatToolbarModule,
+    // MatCardModule,
+    // MatButtonModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule
+  ]
+})
+export class MaterialModule { }
